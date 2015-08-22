b8e836bb3ae982f0f52972222c833048feb5df74
David Farrington <info@davidfarrington.co.uk>
2015-08-22 09:56:16 +0100

    Update setup.sh and remove sublime stuff

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-rm ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings && ln -s ~/workspace/dotfiles/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
-rm ~/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap  && ln -s ~/workspace/dotfiles/Default\ \(Linux\).sublime-keymap  ~/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap
-rm ~/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-mousemap  && ln -s ~/workspace/dotfiles/Default\ \(Linux\).sublime-mousemap  ~/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-mousemap
-rm ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings && ln -s ~/workspace/dotfiles/Package\ Control.sublime-settings ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
-rm ~/.config/terminator/config && ln -s ~/workspace/dotfiles/terminator_config ~/.config/terminator/config
+rm ~/.bashrc && ln -s .bashrc ~/.bashrc
+rm ~/.bash_aliases && ln -s .bash_aliases ~/.bash_aliases
+rm ~/.gitconfig && ln -s .gitconfig ~/.gitconfig
+rm ~/.gitignore && ln -s .gitignore ~/.gitignore
+rm ~/.noserc && ln -s .noserc ~/.noserc
+
